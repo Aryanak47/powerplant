c13551cf56970ab0e583ac9ca1c63d9fc625edfa
Yougal Bimali <aryanbimali45@gmail.com>
2023-08-09 16:36:02 +0545

    - fix deployment jar issue

--- src/test/java/com/proshore/powerplant/MainTests.java
+++ src/test/java/com/proshore/powerplant/MainTests.java
@@ -22,7 +22,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-class PowerplantApplicationTests {
+class MainTests {
 
     private BatteryUsecase batteryUsecase;
 
